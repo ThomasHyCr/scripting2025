6da06cbef035e29ef1f39e7f777742a7948d1328
B09S104est <B09S104est@MEDAULAS.UPB>
2025-02-07 12:37:37 -0500

    ui funcional

---
+++ Assets/Proyecto/Scripts/MenuManager.cs
@@ -0,0 +1,59 @@
+using MoreMountains.Tools;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject creditsP;
+    public GameObject instructionsP;
+    public GameObject creditsB;
+    public GameObject instructionsB;
+    public GameObject exitB;
+    /*
+    public GameObject backBI;
+    public GameObject backBC;*/
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AbreMenuIns()
+    {
+        instructionsP.SetActive(true);
+    }
+
+    public void AbreMenuCred() 
+    {
+        creditsP.SetActive(true);
+    }
+
+    public void CerrarMenuCred()
+    {
+        creditsP.SetActive(false);
+    }
+
+    public void CerrarMenuIns()
+    {
+        instructionsP.SetActive(false);
+    }
+
+    public void CargaScene()
+    {
+        MMSceneLoadingManager.LoadScene("LvL1");
+    }
+
+    public void CerrarJuego()
+    {
+        Application.Quit();
+    }
+}
